4b5b1b8b757e832b8eec0fcd1996a8dda59b5a32
Baek Gi Chan <11618nathan@gmail.com>
2018-03-25 16:51:51 +0900

    Update Contacts.c

--- Contacts/Contacts.c
+++ Contacts/Contacts.c
@@ -66,6 +66,30 @@ int main(void)
 	return 0;
 }
 
+
+// 삽입
+void insert(User* ptr, int* num)
+{ 
+    //유저정보가 꽉 차지 않으면 
+    if (*num < MAX) 
+    {  
+        printf("추가할 이름: ");
+        scanf("%s", ptr[*num].name);
+        printf("추가할 전화번호: ");
+        scanf("%s", ptr[*num].number);
+        printf("추가할 주소: ");
+        scanf("%s", ptr[*num].add);
+        printf("추가할 생일: ");
+        scanf("%s", ptr[*num].bir); 
+        (*num)++;
+        printf("------ 추가 완료 ------ \n");
+    }
+    // 공간X 
+    else
+        printf("FULL \n");
+}
+
+
 // 삭제
 int deleted(User* ptr, int* num)
 {
